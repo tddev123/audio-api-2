a94748e52f84eb6ccd0726afef86dd2bf864a689
tddev123 <154409493+tddev123@users.noreply.github.com>
2025-02-03 23:34:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
+    allow_credentials=True,  # Allow credentials (cookies)
     allow_methods=["*"],
     allow_headers=["*"],
 )
