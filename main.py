96d2952bc60ecf6822de3d3c3894399500757c5c
tddev123 <154409493+tddev123@users.noreply.github.com>
2025-02-04 01:38:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3001"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
